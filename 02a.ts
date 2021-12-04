06d2432a0bee35ddfbd1af6cdacdbbbab97d8d3f
Lucio Paiva <luciopaiva@gmail.com>
2021-12-04 12:42:02 +0000

    Day 2a

--- 02a.ts
+++ 02a.ts
@@ -3,3 +3,24 @@ import readLines from "./util/text-file";
 
 const lines = readLines("input/02.txt");
 
+const position = { x: 0, y: 0 };
+
+function getNumber(str: string): number {
+    const match = str.match(/(\d+)/);
+    if (match) {
+        return Number(match[1]);
+    }
+    throw new Error(`String does not contain number: "${str}".`);
+}
+
+for (const line of lines) {
+    switch (line[0]) {
+        case "f": position.x += getNumber(line); break;
+        case "d": position.y += getNumber(line); break;
+        case "u": position.y -= getNumber(line); break;
+    }
+}
+
+console.info(`x: ${position.x}`);
+console.info(`y: ${position.y}`);
+console.info(`Answer: ${position.x * position.y}`);
