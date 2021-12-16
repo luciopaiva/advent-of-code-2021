68a41178c63e7e43726283f7c107bd4f72bb2d26
Lucio Paiva <luciopaiva@gmail.com>
2021-12-16 13:10:44 +0000

    Day 16: refactor to have operation map

--- src/16.ts
+++ src/16.ts
@@ -1,6 +1,28 @@
 
 import * as fs from "fs";
 
+const OPERATIONS: ((...number) => number)[] = [
+    (...t) => t.reduce((sum, term) => sum + term, 0),
+    (...t) => t.reduce((product, factor) => product * factor, 1),
+    Math.min,
+    Math.max,
+    a => a,
+    (a, b) => a > b ? 1 : 0,
+    (a, b) => a < b ? 1 : 0,
+    (a, b) => a === b ? 1 : 0,
+];
+
+const EXPRESSIONS: ((...string) => string)[] = [
+    (...t) => `add(${t.join(", ")})`,
+    (...t) => `mul(${t.join(", ")})`,
+    (...t) => `min(${t.join(", ")})`,
+    (...t) => `max(${t.join(", ")})`,
+    a => `${a}`,
+    (a, b) => `gt(${a}, ${b})`,
+    (a, b) => `lt(${a}, ${b})`,
+    (a, b) => `eq(${a}, ${b})`,
+];
+
 class PacketDecoder {
 
     private p: number = 0;
@@ -9,72 +31,16 @@ class PacketDecoder {
     constructor(private readonly bits: string) {
     }
 
-    read(): number {
+    read(): [number, string] {
         return this.readPacket();
     }
 
-    readPacket(): number {
+    readPacket(): [number, string] {
         const version = this.read3BitValue();
         const type = this.read3BitValue();
         this.versionSum += version;
-        console.info("Packet version " + version + ", type " + type);
 
-        switch (type) {
-            case 0: return this.readSum();
-            case 1: return this.readProduct();
-            case 2: return this.readMin();
-            case 3: return this.readMax();
-            case 4: return this.readLiteral();
-            case 5: return this.readGreaterThan();
-            case 6: return this.readLessThan();
-            case 7: return this.readEqualTo();
-            default: throw new Error(`Invalid packet type ${type}`);
-        }
-    }
-
-    readSum() {
-        const result = [...this.readOperator()].reduce((sum, term) => sum + term, 0);
-        console.info("Sum " + result);
-        return result;
-    }
-
-    readProduct() {
-        const result = [...this.readOperator()].reduce((product, factor) => product * factor, 1);
-        console.info("Product " + result);
-        return result;
-    }
-
-    readMin() {
-        const result = Math.min(...this.readOperator());
-        console.info("Min " + result);
-        return result;
-    }
-
-    readMax() {
-        const result = Math.max(...this.readOperator());
-        console.info("Min " + result);
-        return result;
-    }
-
-    readGreaterThan() {
-        const [a, b] = [...this.readOperator()];
-        const result = a > b ? 1 : 0;
-        console.info("Greater than " + result);
-        return result;
-    }
-
-    readLessThan() {
-        const [a, b] = [...this.readOperator()];
-        const result = a < b ? 1 : 0;
-        console.info("Less than " + result);
-        return result;
-    }
-
-    readEqualTo() {
-        const [a, b] = [...this.readOperator()];
-        const result = a === b ? 1 : 0;
-        console.info("Equal to " + result);
-        return result;
+        return type === 4 ? this.readLiteral() : this.readPacketType(type);
     }
 
     readBits(l: number) {
@@ -83,31 +49,44 @@ class PacketDecoder {
         return bits;
     }
 
-    *readOperator(): Generator<number> {
+    readPacketType(type: number): [number, string] {
         const lengthType = this.readBits(1);
         if (lengthType === "0") {
             const length = parseInt(this.readBits(15), 2);
-            const limit = this.p + length;
-            while (this.p < limit) {
-                yield this.readPacket();
-            }
+            return this.processOperation(type, ...this.readOperandsByLength(length));
         } else {
             const packetCount = parseInt(this.readBits(11), 2);
-            for (let i = 0; i < packetCount; i++) {
-                yield this.readPacket();
-            }
+            return this.processOperation(type, ...this.readOperandsByCount(packetCount));
         }
     }
 
-    readLiteral() {
+    *readOperandsByLength(length: number): Generator<[number, string]> {
+        const limit = this.p + length;
+        while (this.p < limit) {
+            yield this.readPacket();
+        }
+    }
+
+    *readOperandsByCount(count: number): Generator<[number, string]> {
+        for (let i = 0; i < count; i++) {
+            yield this.readPacket();
+        }
+    }
+
+    readLiteral(): [number, string] {
         let chunk = "", flag;
         do {
             flag = this.readBits(1);
             chunk += this.readBits(4);
         } while (flag === "1");
         const literal = parseInt(chunk, 2);
-        console.info("Literal " + literal);
-        return literal;
+        return this.processOperation(4, [literal, literal.toString()]);
+    }
+
+    processOperation(type: number, ...terms: [number, string][]): [number, string] {
+        const values = terms.map(t => t[0]);
+        const expressions = terms.map(t => t[1]);
+        return [OPERATIONS[type](...values), EXPRESSIONS[type](...expressions)];
     }
 
     read3BitValue() {
@@ -119,9 +98,10 @@ class PacketDecoder {
             .map(X => parseInt(X, 16)).map(n => n.toString(2).padStart(4, "0")).join("");
 
         const decoder = new PacketDecoder(bits);
-        const result = decoder.read();
+        const [result, expression] = decoder.read();
         console.info(`[${fileName}] version sum (part 1): ${decoder.versionSum}`);
         console.info(`[${fileName}] result (part 2): ${result}`);
+        console.info(`[${fileName}] expression: ${expression}`)
     }
 
     static run(fileName: string) {
