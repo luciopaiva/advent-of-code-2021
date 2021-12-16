851e7c203ab26dc46ca158c206292f5f5e76b085
Lucio Paiva <luciopaiva@gmail.com>
2021-12-16 12:00:39 +0000

    Day 16b

--- src/16.ts
+++ src/16.ts
@@ -1,5 +1,6 @@
 
 import * as fs from "fs";
+import {sum} from "./utils";
 
 class PacketDecoder {
 
@@ -9,16 +10,72 @@ class PacketDecoder {
     constructor(private readonly bits: string) {
     }
 
-    readPacket() {
+    read(): number {
+        return this.readPacket();
+    }
+
+    readPacket(): number {
         const version = this.read3BitValue();
         const type = this.read3BitValue();
         this.versionSum += version;
         console.info("Packet version " + version + ", type " + type);
-        if (type === 4) {
-            this.readLiteral();
-        } else {
-            this.readOperator(type);
+
+        switch (type) {
+            case 0: return this.readSum();
+            case 1: return this.readProduct();
+            case 2: return this.readMin();
+            case 3: return this.readMax();
+            case 4: return this.readLiteral();
+            case 5: return this.readGreaterThan();
+            case 6: return this.readLessThan();
+            case 7: return this.readEqualTo();
+            default: throw new Error(`Invalid packet type ${type}`);
         }
+    }
+
+    readSum() {
+        const result = sum([...this.readOperator()]);
+        console.info("Sum " + result);
+        return result;
+    }
+
+    readProduct() {
+        const result = [...this.readOperator()].reduce((product, term) => product * term, 1);
+        console.info("Product " + result);
+        return result;
+    }
+
+    readMin() {
+        const result = Math.min(...this.readOperator());
+        console.info("Min " + result);
+        return result;
+    }
+
+    readMax() {
+        const result = Math.max(...this.readOperator());
+        console.info("Min " + result);
+        return result;
+    }
+
+    readGreaterThan() {
+        const [a, b] = [...this.readOperator()];
+        const result = a > b ? 1 : 0;
+        console.info("Greater than " + result);
+        return result;
+    }
+
+    readLessThan() {
+        const [a, b] = [...this.readOperator()];
+        const result = a < b ? 1 : 0;
+        console.info("Less than " + result);
+        return result;
+    }
+
+    readEqualTo() {
+        const [a, b] = [...this.readOperator()];
+        const result = a === b ? 1 : 0;
+        console.info("Equal to " + result);
+        return result;
     }
 
     readBits(l: number) {
@@ -27,19 +84,18 @@ class PacketDecoder {
         return bits;
     }
 
-    readOperator(operator: number) {
-        console.info("Operator " + operator);
+    *readOperator(): Generator<number> {
         const lengthType = this.readBits(1);
         if (lengthType === "0") {
             const length = parseInt(this.readBits(15), 2);
             const limit = this.p + length;
             while (this.p < limit) {
-                this.readPacket();
+                yield this.readPacket();
             }
         } else {
             const packetCount = parseInt(this.readBits(11), 2);
             for (let i = 0; i < packetCount; i++) {
-                this.readPacket();
+                yield this.readPacket();
             }
         }
     }
@@ -59,14 +115,18 @@ class PacketDecoder {
         return parseInt(this.readBits(3), 2);
     }
 
-    static run(fileName: string) {
-        const input = fs.readFileSync(fileName, "utf-8")
+    static runInput(input: string, fileName: string = "?") {
         const bits = input.replace(/\s/g, "").split("")
             .map(X => parseInt(X, 16)).map(n => n.toString(2).padStart(4, "0")).join("");
 
         const decoder = new PacketDecoder(bits);
-        decoder.readPacket();
-        console.info(`[${fileName}] version sum: ${decoder.versionSum}`);
+        const result = decoder.read();
+        console.info(`[${fileName}] version sum (part 1): ${decoder.versionSum}`);
+        console.info(`[${fileName}] result (part 2): ${result}`);
+    }
+
+    static run(fileName: string) {
+        PacketDecoder.runInput(fs.readFileSync(fileName, "utf-8"), fileName);
     }
 }
 
