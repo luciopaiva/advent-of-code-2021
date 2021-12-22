56a9dadb3483c271600ad4e57c20e664696e7475
Lucio Paiva <luciopaiva@gmail.com>
2021-12-22 01:58:21 +0000

    Refactor cache

--- src/21b.ts
+++ src/21b.ts
@@ -8,9 +8,24 @@ interface State {
     wins2: number,
 }
 
-const cache = new Map<string, State>();
-let cacheHits = 0;
-let cacheAccesses = 0;
+class Cache extends Map<string, State> {
+    public cacheHits = 0;
+    public cacheAccesses = 0;
+
+    key(...args) {
+        return args.map(arg => arg.toString()).join(".");
+    }
+
+    get(key: string): State {
+        const value = super.get(key);
+        if (value) {
+            this.cacheHits++;
+        }
+        this.cacheAccesses++;
+        return value;
+    }
+}
+const cache = new Cache();
 
 const diceRolls = new Counter<number>();
 
@@ -25,11 +40,9 @@ for (let i = 1; i < 4; i++) {
 const mov = (pos: number): number => pos % 10;
 
 function round(turn: boolean, pos1: number, pos2: number, pts1: number, pts2: number): State {
-    const key = `${turn}.${pos1}.${pos2}.${pts1}.${pts2}`;
+    const key = cache.key(...arguments);
     const value = cache.get(key);
-    cacheAccesses++;
     if (value) {
-        cacheHits++;
         return value;
     }
 
@@ -37,11 +50,11 @@ function round(turn: boolean, pos1: number, pos2: number, pts1: number, pts2: nu
         wins1: 0,
         wins2: 0,
     };
+    cache.set(key, result);
 
     if (pts1 >= MAX || pts2 >= MAX) {
         // console.info(`p1:(${pos1},${pts1}) p2:(${pos2},${pts2})`);
         pts1 > pts2 ? result.wins1 = 1 : result.wins2 = 1;
-        cache.set(key, result);
         return result;
     }
 
@@ -58,16 +71,14 @@ function round(turn: boolean, pos1: number, pos2: number, pts1: number, pts2: nu
         result.wins2 += sub.wins2 * times;
     }
 
-    cache.set(key, result);
     return result;
 }
 
 function run(name: string, pos1: number, pos2: number) {
-    cacheHits = 0;
-    cacheAccesses = 0;
+    cache.clear();
     const result = round(true, pos1 - 1, pos2 - 1, 0, 0);
-    const perc = Math.round(100 * cacheHits / cacheAccesses);
-    console.info(`[${name}] Cache: total=${cacheAccesses}, hits=${cacheHits} (${perc}%)`);
+    const perc = Math.round(100 * cache.cacheHits / cache.cacheAccesses);
+    console.info(`[${name}] Cache: total=${cache.cacheAccesses}, hits=${cache.cacheHits} (${perc}%)`);
     const max = Math.max(result.wins1, result.wins2);
     console.info(`[${name}] p1=${result.wins1} x p2=${result.wins2} -> ${max}`);
 }
