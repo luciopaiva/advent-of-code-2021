0ef0f8c8ebeab6c1564a28c0f877dc408f94da16
Lucio Paiva <luciopaiva@gmail.com>
2021-12-22 21:35:33 +0000

    Day 22b

--- src/22a.ts
+++ src/22a.ts
@@ -5,20 +5,13 @@ function encode(x: number, y: number, z: number): string {
     return `${x}.${y}.${z}`;
 }
 
-function decode(s: string): [number, number, number] {
-    return s.split(".").map(n => parseInt(n)) as [number, number, number];
-}
-
 class Reactor {
     public cubes: Set<string> = new Set();
 
     cuboid(on: boolean, coords: number[]) {
         for (let x = coords[0]; x <= coords[1]; x++) {
-            // if (!this.withinBounds(x)) continue;
             for (let y = coords[2]; y <= coords[3]; y++) {
-                // if (!this.withinBounds(y)) continue;
                 for (let z = coords[4]; z <= coords[5]; z++) {
-                    // if (!this.withinBounds(y)) continue;
                     on ? this.cubes.add(encode(x, y, z)) : this.cubes.delete(encode(x, y, z));
                 }
             }
@@ -35,7 +28,6 @@ async function run(fileName: string) {
             reactor.cuboid(m[1] === "on", coords);
         }
     }
-
 
     console.info(`[${fileName}] cubes on: ${reactor.cubes.size}`);
 }
---
+++ src/22b.ts
@@ -0,0 +1,86 @@
+
+import {readLines} from "./utils";
+
+class Cuboid {
+
+    public readonly size: number;
+
+    constructor(
+        public readonly label: string,
+        public readonly x0: number,
+        public readonly x1: number,
+        public readonly y0: number,
+        public readonly y1: number,
+        public readonly z0: number,
+        public readonly z1: number,
+        public readonly sign: boolean,
+    ) {
+        this.size = Math.abs((x1 - x0 + 1) * (y1 - y0 + 1) * (z1 - z0 + 1));
+    }
+
+    intersect(other: Cuboid): Cuboid {
+        const [x0, x1] = [Math.max(this.x0, other.x0), Math.min(this.x1, other.x1)];
+        const [y0, y1] = [Math.max(this.y0, other.y0), Math.min(this.y1, other.y1)];
+        const [z0, z1] = [Math.max(this.z0, other.z0), Math.min(this.z1, other.z1)];
+        if (x1 < x0 || y1 < y0 || z1 < z0) {
+            return undefined;
+        }
+        return new Cuboid(`${this.label} ∩ ${other.label}`, x0, x1, y0, y1, z0, z1, !this.sign);
+    }
+
+    count(): number {
+        return this.sign ? this.size : -this.size;
+    }
+
+    toString(): string {
+        const op = this.sign ? "ADD" : "SUB";
+        return `${op} x=${this.x0}..${this.x1},y=${this.y0}..${this.y1},z=${this.z0}..${this.z1}`;
+    }
+}
+
+class Reactor {
+    public size = 0;
+    public cuboids: Cuboid[] = [];
+
+    add(cuboid: Cuboid) {
+        const changes: Cuboid[] = [];
+
+        cuboid.sign && changes.push(cuboid);
+
+        for (const other of this.cuboids) {
+            const intersection = other.intersect(cuboid);
+            intersection && changes.push(intersection);
+        }
+
+        for (const cuboid of changes) {
+            this.cuboids.push(cuboid);
+            this.size += cuboid.count();
+        }
+    }
+
+    toString() {
+        const terms = [this.cuboids[0].label];
+        terms.push(...this.cuboids.slice(1).map(c => `${c.sign ? "+" : "-"} ${c.label}`));
+        return terms.join(" ");
+    }
+}
+
+async function run(fileName: string) {
+    const reactor = new Reactor();
+    let id = 1;
+    const range = "(-?[\\d]+)\\.\\.(-?[\\d]+)";
+    const re = new RegExp(`(on|off).x=${range}.y=${range}.z=${range}`);
+    for await (const line of readLines(fileName)) {
+        const m = line.match(re);
+        const [a, b, c, d, e, f] = m.slice(2).map(n => Number(n));
+        reactor.add(new Cuboid(`C${id++}`, a, b, c, d, e, f, m[1] === "on"));
+    }
+
+    console.info(reactor.toString());
+    console.info(`[${fileName}] resulting cuboids: ${reactor.cuboids.length}`);
+    console.info(`[${fileName}] cubes turned on: ${reactor.size}`);
+}
+
+await run("input/22-example.txt");
+// await run("input/22.txt");
+
